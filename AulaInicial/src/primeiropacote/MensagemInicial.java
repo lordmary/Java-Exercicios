62360ebb619a941df3a01ade1e2931bf3a4516e4
Maria Fernanda Gouveia <57708477+lordmary@users.noreply.github.com>
2021-12-15 19:38:05 -0300

    Add files via upload

---
+++ AulaInicial/src/primeiropacote/MensagemInicial.java
@@ -0,0 +1,42 @@
+
+package primeiropacote;
+
+
+public class MensagemInicial {
+    
+    public static void  main(String a[]){
+        System.out.println("Primeiro programa em Java");
+        
+    }
+    private int x;
+   private float y;
+
+    /**
+     * @return the x
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * @param x the x to set
+     */
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    /**
+     * @return the y
+     */
+    public float getY() {
+        return y;
+    }
+
+    /**
+     * @param y the y to set
+     */
+    public void setY(float y) {
+        this.y = y;
+    }
+    
+}
